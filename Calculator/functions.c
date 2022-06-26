4dd6b32165bd5b6bcb7fd4d21007cce31ba5314a
Chernobrovka <gomhu72@gmail.com>
2022-06-26 19:44:04 +0300

    feat: write the getOP function

--- Calculator/functions.c
+++ Calculator/functions.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 
+#define NUMBER 1000
+
 
 int string_to_int( char str[]);
 int get_op(char op[]);
 
 int get_op(char op[]){
-    static int 
+    static int symbol = 0;
+    int i = 0;
+    if (symbol == 0) {
+        symbol = getchar();
+    }
+    while (isspace(symbol)){
+        symbol = getchar();
+    }
+    if(!isdigit(symbol)){
+        return symbol;
+    }
+    if (isdigit(symbol)){
+        op[i] = symbol;
+        while(isdigit(op[++i] = ( c = getchar())));
+        op[i] = '\0';
+
+        return NUMBER;
+    }
 }
